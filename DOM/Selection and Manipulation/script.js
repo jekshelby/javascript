526924a59b774e21bb8b0df1e8898a366cd0c2d8
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-05 13:55:19 +0700

    update scripting

--- DOM/Selection and Manipulation/script.js
+++ DOM/Selection and Manipulation/script.js
@@ -1,14 +1,13 @@
 function changeColor(newColor) { //function changeColor with prameter newColor
     const elem = document.getElementById("para"); // get element html with ID para
-    elem.style.color = newColor; //elem.style.color = styling (color)
+    elem.style.color = newColor; //manipulation style.color = styling (color)
 }
 
 // Mendapatkan elemen-elemen dengan class "paragraf"
 const elements = document.getElementsByClassName('paragraf');
 
 // Iterasi melalui semua elemen yang memiliki class "paragraf"
-for (let i = 0; i < elements.length; i++) {
+for (let paragraf of elements) {
     // Mengubah teks elemen menjadi "SIP"
-    elements[i].innerText = "sip";
+    paragraf.innerText = "sip"; //manipulation innertext
 }
-
