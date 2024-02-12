5fe0207af19755bfd3d4ac984f17cb0efab3485f
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-12 14:46:16 +0700

    operator perbandingan

---
+++ dasar/operator/operatorPerbandingan.js
@@ -0,0 +1,19 @@
+console.log("<== Operator Perbandingan ==>");
+
+// >
+// <
+// >=
+// <=
+// ==
+// ===
+// !==
+// !===
+
+// beda == dan === yaitu membedakan tipe data juga
+
+let result = 1 == '1'; // dibandingkan dengan ==, jadi tidak dibandingkan tipe datanya
+console.log(result); // true
+
+let hasil = 1 === '1'; // dibandingkan dengan ===
+console.log(hasil); // false
+
