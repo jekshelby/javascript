e7ca1c933bccef33f8b8779d86b447a093ad3cfe
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-12 12:31:34 +0700

    tipe data string

---
+++ dasar/tipeData/tipeDataString.js
@@ -0,0 +1,16 @@
+console.log("<== Tipe Data String ==>");
+
+console.log("Muhammad Dzaky Ahnaf");
+console.log('Muhammad Dzaky Ahnaf');
+
+console.log("Muhammad Dzaky" + " Ahnaf");
+
+
+// escape sequence
+// \t   tab
+// \'   '
+// \"   "
+// \\   \
+// \n   enter
+
+console.log("\t\'Muhammad \\Dzaky Ahnaf\'\n");
