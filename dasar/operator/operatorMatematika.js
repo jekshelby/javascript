1a9ed7fa9268e8d23a26e70f46affa82de580149
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-12 13:16:02 +0700

    operator matematika

---
+++ dasar/operator/operatorMatematika.js
@@ -0,0 +1,23 @@
+console.log("<== Operator Matematika ==>");
+
+// operator aritmatika
+console.log("Aritmatika");
+let tambah = 1+1;
+let kurang = 1-1;
+let kali = 1*1;
+let bagi = 1/1;
+let pangkat = 2**2;
+let modulus = 5%4;
+
+// operator augmanted assignment
+console.log("Augmanted Assignment");
+// +=, -=, *=, /=, **=, %=
+let hasil;
+hasil += 10; // hasil = hasil + 10;
+
+// operator unary
+console.log("Unary");
+// +, -, ++, --
+// + menandakan nilai positif
+// - menandakan nilai positif
+// ++, -- increment decrement
