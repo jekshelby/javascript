dbedceea23757def88895f96164541aacf4d7bc5
Muhammad Dzaky Ahnaf <ahnafdzaky9@gmail.com>
2024-02-12 12:48:10 +0700

    variable

---
+++ dasar/variabel/variable.js
@@ -0,0 +1,21 @@
+console.log("<== Variabel ==>");
+
+// variabel let
+// variabel dinamis, dapat diubah" valuenya
+
+// variable yang tidak lgsg diberi value
+let nama;
+nama = "Muhammad Dzaky Ahnaf";
+console.log(nama);
+
+// variable yang lgsg diberi value
+let Nama = "Muhammad Dzaky Ahnaf";
+console.log(Nama);
+
+
+// variable const
+// variabel yang sudah dimasukkan tidak bisa diubah"
+const jenisKelamin = "Laki - Laki";
+console.log(jenisKelamin);
+
+// jenisKelamin = "dsfs"; kalau diubah bakalan error
